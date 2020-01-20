1f493165ef7b9a383e6c935dbf0d90622613a8ce
Louis Aussedat <aussedat.louis@gmail.com>
2020-01-20 20:53:58 +0100

    setup.py: adding scripts and requires

--- setup.py
+++ setup.py
@@ -8,5 +8,12 @@ setup(
     author_email='aussedat.louis@gmail.com',
     description='Two factor autentication module',
     packages=find_packages(),
-    # install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+	scripts = [
+		'bin/pyticator-client',
+		'bin/pyticator-server',
+    ],
+    install_requires=[
+        'configparser >= 3.7.0',
+        'crypto >= 1.25.7',
+    ],
 )
